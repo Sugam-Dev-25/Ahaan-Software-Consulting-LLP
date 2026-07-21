83a7d8d9ea773eca8e16adcb0cee1f49d87f25c6
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-07-21 18:07:52 +0530

    Add About Company

--- frontend/src/components/pages/home/HomeComponent.tsx
+++ frontend/src/components/pages/home/HomeComponent.tsx
@@ -8,12 +8,14 @@ import { TotalProject } from "./components/TotalProject"
 import { WhyChooseUs } from "./components/WhyChooseUs"
 import { HomeBanner } from "./components/HomeBanner"
 import { CallToAction } from "./components/CallToAction"
+import { AboutCompany } from "./components/AboutCompany"
 
 export const HomeComponent = () => {
   return (
     <div>
       <HomeBanner/>
       <ServicesSection/>
+      <AboutCompany/>
       <OurTechnology/>
       <WhyChooseUs/>
       <BusinessCard/>
